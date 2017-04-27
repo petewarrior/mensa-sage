ee8349fcebe779e5be8ec2151a401b331facb604
Arya Antaputra <petewarrior@gmail.com>
2017-04-27 11:49:27 +0700

    add Give currency

--- functions.php
+++ functions.php
@@ -36,3 +36,38 @@ function my_eme_add_currencies($currencies){
 	return $currencies;
 }
 add_filter('eme_add_currencies','my_eme_add_currencies');
+
+/**
+ * Adds a new Currency symbol and name to Give payment options
+ * @since 1.0
+ * NOTE: Give supports all currencies that PayPal Standard offers
+ * See here: https://developer.paypal.com/docs/classic/api/country_codes/
+ * 
+ * This adds the currency as an option in your currency settings 
+ * and will output in your front-end forms. But it is up to your 
+ * Payment Gateway to handle that currency correctly. 
+ * This means that even though the form will show your currency, and 
+ * you'll see the reports with this currency, your Payment processor 
+ * may reflect something different.
+ */
+/*
+ * Adds Costa Rican Colon currency to your Give settings
+ */
+add_filter('give_currencies', 'give_add_idr_currency');
+function add_costarican_currency($currencies) {
+    $currencies['IDR'] = __( 'Indonesian Rupiah (Rp)', 'give' );
+    return $currencies;
+}
+/*
+ * Converts the currency code to the correct HTML character symbol
+ * for the form output
+ */
+add_filter('give_currency_symbol', 'add_colon_symbol', 10,2);
+function add_colon_symbol($symbol, $currency) {
+    switch ( $currency ) :
+        case "IDR" :
+            $symbol = 'Rp';
+            break;
+    endswitch;
+    return $symbol;
+}
